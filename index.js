520fb2ee67a941102b2aade0cbcf8665a8d7f0a4
familstorm <ngochoacth53@gmail.com>
2024-08-09 22:36:42 +0700

    #first setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const WebSocket = require('ws');
+
+// Tạo WebSocket server chạy trên cổng 8080
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  // Gửi thông báo chào mừng khi client kết nối
+  ws.send('Welcome to WebSocket server');
+
+  // Xử lý khi nhận được tin nhắn từ client
+  ws.on('message', (message) => {
+    console.log(`Received: ${message}`);
+
+    // Gửi lại thông điệp đã nhận từ client
+    ws.send(`Server received: ${message}`);
+  });
+
+  // Xử lý khi client ngắt kết nối
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    ws.send(`Say goodbye to client`);
+
+  });
+
+  // Xử lý lỗi
+  ws.on('error', (error) => {
+    console.error('WebSocket error:', error);
+  });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
